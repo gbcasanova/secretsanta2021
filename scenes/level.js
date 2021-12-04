0d472a3df3ab418df03454ed8ad335c97ab7b939
Gabriel Casanova <48224013+gbcasanova@users.noreply.github.com>
2021-12-04 15:31:21 -0300

    Added items showcase

--- scenes/level.js
+++ scenes/level.js
@@ -13,33 +13,26 @@ class Level extends Phaser.Scene
 		this.load.tilemapTiledJSON("tilemap", "tilemaps/test.json");
 		
 		this.load.image("tools_selector", "sprites/tools_selector.png")
-		this.load.image("tools_selector_outline", "sprites/tools_selector_outline.png")
+		this.load.image("outline", "sprites/outline.png")
+		this.load.image("items_showcase", "sprites/items_showcase.png")
     }
-
-    create()
-    {
-		this.input.setDefaultCursor('url(sprites/cursors/arrow.cur), pointer');
-		
-		// Tilemap.
-		const map = this.make.tilemap({key: "tilemap"});
-		const tileset = map.addTilesetImage("tileset", "tileset");
-		map.createLayer("ground", tileset)
-		map.createLayer("deco", tileset)
-		
-		this.player = new Player(this, 90, 90);
-		
-		// GUI.
+	
+	create_gui()
+	{
 		this.current_tool = 0; // 0 - Pickaxe | 1 - Shovel | 2 - Axe | 3 - Bucket
 		
+		// Tools sprite.
 		this.tools_selector = this.add.sprite(config.width/2, config.height - 64, "tools_selector");
 		this.tools_selector.setScrollFactor(0); // Tools image.
 		
-		this.tools_selector_outline = this.add.sprite(config.width/2 + 64*this.current_tool, config.height-64, "tools_selector_outline");
-		this.tools_selector_outline.setScrollFactor(0); // Tools outline.
-	
+		// Outline sprite.
+		this.outline = this.add.sprite(128 + 64*this.current_tool, 384, "outline")
+			.setScrollFactor(0)
+			.setOrigin(0, 0);
+		
+		// Wheel selection.
 		this.input.on('wheel', function(pointer, currentlyOver, dx, dy, dz, event)
 		{
-			// Wheel selection.
 			this.scene.current_tool += Math.sign(dy);
 			
 			if (this.scene.current_tool < 0)
@@ -47,11 +40,44 @@ class Level extends Phaser.Scene
 			else if (this.scene.current_tool > 3)
 				this.scene.current_tool = 3;
 		});
+		
+		// Items showcase sprite.
+		this.items_showcase = this.add.sprite(10, 10, "items_showcase")
+			.setScrollFactor(0)
+			.setOrigin(0, 0);		
+	}
+	
+	update_gui()
+	{
+		// Change outline position based on tool.
+        this.outline.x = 128 + 64*this.current_tool
+		
+		if (this.current_tool == 0)
+			this.input.setDefaultCursor('url(sprites/cursors/pickaxe.cur), pointer');
+		else if (this.current_tool == 1)
+			this.input.setDefaultCursor('url(sprites/cursors/shovel.cur), pointer');
+		else if (this.current_tool == 2)
+			this.input.setDefaultCursor('url(sprites/cursors/axe.cur), pointer');
+		else if (this.current_tool == 3)
+			this.input.setDefaultCursor('url(sprites/cursors/bucket.cur), pointer');
+	}
 
+    create()
+    {
+		
+		// Tilemap.
+		const map = this.make.tilemap({key: "tilemap"});
+		const tileset = map.addTilesetImage("tileset", "tileset");
+		map.createLayer("ground", tileset);
+		map.createLayer("deco", tileset);
+		
+		this.player = new Player(this, 90, 90);;
+		
+		this.create_gui();
     }
 
     update(time, delta)
     {
-        this.tools_selector_outline.x = config.width/2 + 64*this.current_tool
+		this.update_gui();
     }
 }
