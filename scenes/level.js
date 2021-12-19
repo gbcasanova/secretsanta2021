fafbd278d4cac467e62e29f6ac6e0356d35b27b9
Gabriel Casanova <con.casanovaproductions@gmail.com>
2021-12-19 01:20:54 -0300

    added cache removal so things don't crap out

--- scenes/level.js
+++ scenes/level.js
@@ -7,6 +7,8 @@ class CraftLevel extends Phaser.Scene
 
     preload()
     {
+		this.cache.tilemap.remove("tilemap")
+		
 		// Level sprites.
         this.load.spritesheet("fallguy",   "sprites/fallguy.png",   {frameWidth: 66, frameHeight: 82});
 		this.load.spritesheet("trees",     "sprites/trees.png",     {frameWidth: 128,frameHeight: 192});
