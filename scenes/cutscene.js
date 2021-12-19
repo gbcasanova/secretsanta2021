b0ca55c4c8c9597fd42af50c47be40f73a1a1439
Gabriel Casanova <con.casanovaproductions@gmail.com>
2021-12-19 01:20:05 -0300

    New cutscene dialogue

--- scenes/cutscene.js
+++ scenes/cutscene.js
@@ -7,6 +7,9 @@ class Cutscene extends Phaser.Scene
 
     preload()
     {
+		this.cache.audio.remove("cutscene0");
+		this.cache.audio.remove("cutscene1");
+		
         this.load.spritesheet("cutscenes", "sprites/gui/cutscenes.png", {frameWidth: 512, frameHeight: 480});
 		this.load.spritesheet("cursors", "sprites/gui/cursors.png", {frameWidth: 34, frameHeight: 34})
 		
@@ -17,20 +20,26 @@ class Cutscene extends Phaser.Scene
 				this.load.audio("cutscene0", "sounds/cutscenes/cutscene0_0.mp3")
 				this.load.audio("cutscene1", "sounds/cutscenes/cutscene0_1.mp3")
 				break;
+			
+			case 1:
+				this.load.audio("cutscene0", "sounds/cutscenes/cutscene1_0.mp3")
+				this.load.audio("cutscene1", "sounds/cutscenes/cutscene1_1.mp3")
+				break;
 		}
 		
     }
 
     create()
     {
+		let cutscene_number = level_game*2
+		
 		this.sound.stopAll();
 		this.cameras.main.fadeIn(1000);
-		this.cutscene_sprite = this.add.sprite(0,0, "cutscenes", 0)
+		this.cutscene_sprite = this.add.sprite(0,0, "cutscenes", cutscene_number)
 			.setOrigin(0, 0)
 			
 		// Cursor.
 		this.sys.canvas.style.cursor = 'none';
-		this.cursor = new Cursor(this);
 		
 		// Select the specific sound.
 		this.cutsceneaudio_0 = this.sound.add("cutscene0");
@@ -38,7 +47,7 @@ class Cutscene extends Phaser.Scene
 		
 		this.cutsceneaudio_0.play(); // Play first audio.
 		this.cutsceneaudio_0.on('complete', function(){
-			this.cutscene_sprite.setFrame(1)
+			this.cutscene_sprite.setFrame(cutscene_number + 1)
 			this.cutsceneaudio_1.play()
 		}, this);
 		
