621412a9c0ddf09293f124da0ed2e8c36b2fa49c
Gabriel Casanova <con.casanovaproductions@gmail.com>
2021-12-19 22:21:55 -0300

    Added icon

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function createWindow() {
         height: 960,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+		icon: __dirname + '/favicon.ico'
     })
     win.loadFile('index.html');
 	win.setMenuBarVisibility(false);
